a39cd02c76809be823bfa8e8540fb78bece708a3
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-05-29 17:59:24 +0300

    Tehtävän 3.8 valmistuminen

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
 //const res = require('express/lib/response')
+
+morgan.token('body', (req, res) => { 
+    if (req.method === "POST") { 
+        return JSON.stringify(req.body) 
+    }
+    return '' 
+})
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {

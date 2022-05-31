1bb06a39cde895d3e4a0b2e51aa2999189c78e62
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-05-31 18:11:09 +0300

    Tehtävän 3.15 valmistuminen

--- index.js
+++ index.js
@@ -116,9 +116,13 @@ app.post('/api/persons', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => {
+            console.log(error)
+        })
 })
 
 const PORT = process.env.PORT
